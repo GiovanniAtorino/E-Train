c240e88666fa8abe723f0b7883ae1d4ce430b35d
raffaelestefanile8 <32306357+raffaelestefanile8@users.noreply.github.com>
2018-01-17 12:57:03 +0100

    filesql

---
+++ WebContent/etrain.sql
@@ -0,0 +1,193 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win32 (AMD64)
+--
+-- Host: localhost    Database: etrain
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `azienda`
+--
+
+DROP TABLE IF EXISTS `azienda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `azienda` (
+  `nome` varchar(25) NOT NULL,
+  `sede` varchar(25) DEFAULT NULL,
+  `nome_tirocineo` varchar(25) DEFAULT NULL,
+  `nome_stage` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `azienda`
+--
+
+LOCK TABLES `azienda` WRITE;
+/*!40000 ALTER TABLE `azienda` DISABLE KEYS */;
+/*!40000 ALTER TABLE `azienda` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `segreteria`
+--
+
+DROP TABLE IF EXISTS `segreteria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `segreteria` (
+  `nome` varchar(25) NOT NULL,
+  `dipartimento` varchar(25) DEFAULT NULL,
+  `facolta` varchar(25) DEFAULT NULL,
+  `num_telefono` varchar(15) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `segreteria`
+--
+
+LOCK TABLES `segreteria` WRITE;
+/*!40000 ALTER TABLE `segreteria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `segreteria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `studente`
+--
+
+DROP TABLE IF EXISTS `studente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `studente` (
+  `matricola` char(5) NOT NULL,
+  `nome` varchar(20) DEFAULT NULL,
+  `cognome` varchar(20) DEFAULT NULL,
+  `nome_azienda` varchar(25) DEFAULT NULL,
+  `nome_tirocineo` varchar(25) DEFAULT NULL,
+  `email` varchar(25) DEFAULT NULL,
+  `username` varchar(25) DEFAULT NULL,
+  `password` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`matricola`),
+  KEY `nome_azienda` (`nome_azienda`),
+  KEY `nome_tirocineo` (`nome_tirocineo`),
+  CONSTRAINT `studente_ibfk_1` FOREIGN KEY (`nome_azienda`) REFERENCES `azienda` (`nome`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `studente_ibfk_2` FOREIGN KEY (`nome_tirocineo`) REFERENCES `tirocineo` (`nome`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studente`
+--
+
+LOCK TABLES `studente` WRITE;
+/*!40000 ALTER TABLE `studente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `studente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tirocineo`
+--
+
+DROP TABLE IF EXISTS `tirocineo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tirocineo` (
+  `nome` varchar(25) NOT NULL,
+  `descrizione` varchar(500) DEFAULT NULL,
+  `data_inizio` date DEFAULT NULL,
+  `data_fine` date DEFAULT NULL,
+  `nome_azienda` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`nome`),
+  KEY `nome_azienda` (`nome_azienda`),
+  CONSTRAINT `tirocineo_ibfk_1` FOREIGN KEY (`nome_azienda`) REFERENCES `azienda` (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tirocineo`
+--
+
+LOCK TABLES `tirocineo` WRITE;
+/*!40000 ALTER TABLE `tirocineo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tirocineo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tutor`
+--
+
+DROP TABLE IF EXISTS `tutor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tutor` (
+  `nome` varchar(20) DEFAULT NULL,
+  `matricola_tutor` char(5) NOT NULL,
+  `cognome` varchar(20) DEFAULT NULL,
+  `email` varchar(25) DEFAULT NULL,
+  `username` varchar(25) DEFAULT NULL,
+  `password` varchar(25) DEFAULT NULL,
+  `nome_azienda` varchar(25) DEFAULT NULL,
+  `nome_tirocineo` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`matricola_tutor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tutor`
+--
+
+LOCK TABLES `tutor` WRITE;
+/*!40000 ALTER TABLE `tutor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tutor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utente`
+--
+
+DROP TABLE IF EXISTS `utente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utente` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `amministratore` enum('si','no') DEFAULT 'no',
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utente`
+--
+
+LOCK TABLES `utente` WRITE;
+/*!40000 ALTER TABLE `utente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `utente` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-25 16:30:32
